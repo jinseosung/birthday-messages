1b961f8db79f50b2fcef3b3a10c595f2c196d9f6
Jinseo Sung <jinseosung15@gmail.com>
2023-11-06 17:32:45 +0100

    create main page

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">HB messages app</div>;
-}
-
-export default App;
---
+++ src/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <div className="main">
+      <button className="main__btn main__btn--ko">한국어</button>
+      <button className="main__btn main__btn--fr">Français</button>
+    </div>
+  );
+}
+
+export default Main;

581f56683f7bb277c0ddd6c316dd1831ed2f3b6d
Jinseo Sung <jinseosung15@gmail.com>
2023-11-07 14:41:04 +0100

    message data setting with firebase

--- src/pages/Message.js
+++ src/pages/Message.js
@@ -1,20 +1,66 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Message = ({ datas }) => {
   const [selectedData, setSelectedData] = useState({});
+  const [leavedMessage, setLeavedMessage] = useState("");
+  const [name, setName] = useState("");
   const { home } = selectedData || {};
   const params = useParams();
   const lanId = params.languageId;
   const navigate = useNavigate();
+  const monthList = [
+    "Jan",
+    "Fév",
+    "Mar",
+    "Avr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Aoû",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Déc",
+  ];
+
+  const date = new Date();
+
+  const dateObj = {
+    minutes: date.getMinutes(),
+    hours: date.getHours(),
+    day: date.getDate(),
+    month: monthList[date.getMonth()],
+    year: date.getFullYear(),
+  };
+
+  const onChange = (e) => {
+    const target = e.target;
+    target.nodeName === "TEXTAREA"
+      ? setLeavedMessage(target.value)
+      : setName(target.value);
+  };
 
   useEffect(() => {
     const lanData = datas.find((data) => data.lanCode === lanId);
     setSelectedData(lanData);
   }, [datas, lanId]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    try {
+      await addDoc(collection(db, "messages"), {
+        name: name,
+        message: leavedMessage,
+        dateObj,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+
     navigate(`/thx/${lanId}`);
   };
 
@@ -23,10 +69,15 @@ const Message = ({ datas }) => {
       <div className="message">
         <h1 className="message__title">{home.title}</h1>
         <form onSubmit={handleSubmit}>
-          <textarea className="message__area" required></textarea>
+          <textarea
+            onChange={onChange}
+            className="message__area"
+            required
+          ></textarea>
           <div className="message__nameContainer">
             <label>{home.from}</label>
             <input
+              onChange={onChange}
               className="message__nameInput"
               placeholder={home.placeholder}
               required

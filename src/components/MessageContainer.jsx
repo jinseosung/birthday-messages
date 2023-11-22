00147188a2c1cec8b72e3da608460b16e0734ee7
Jinseo Sung <jinseosung15@gmail.com>
2023-11-22 11:00:00 +0000

    add MessageContainer component

---
+++ src/components/MessageContainer.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
+
+export default function MessageContainer({ message }) {
+  return (
+    <div key={message.id} className="messageContainer">
+      <div className="messageContainer__top">
+        <div className="messageContainer__title">
+          <FontAwesomeIcon
+            icon={faComment}
+            className="messageContainer__icon"
+          />
+          <span className="messageContainer__name">{message.name}</span>
+        </div>
+        <span className="messageContainer__date">
+          {`${message.dateObj.hours}:${message.dateObj.minutes}, ${message.dateObj.day} ${message.dateObj.month}`}
+        </span>
+      </div>
+      <div className="messageContainer__bottom">
+        <span className="messageContainer__message">{message.message}</span>
+      </div>
+    </div>
+  );
+}
--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -4,8 +4,9 @@ import { db } from "../firebase";
 import { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faComment } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import BtnRose from "../components/BtnRose";
+import MessageContainer from "../components/MessageContainer";
 
 const Messages = () => {
   const [messageList, setMessageList] = useState([]);
@@ -29,20 +30,7 @@ const Messages = () => {
         </Link>
       </div>
       {messageList.map((message) => (
-        <div key={message.id} className="messages__container">
-          <div className="messages__top">
-            <div className="messages__title">
-              <FontAwesomeIcon icon={faComment} className="messages__icon" />
-              <span className="messages__name">{message.name}</span>
-            </div>
-            <span className="messages__date">
-              {`${message.dateObj.hours}:${message.dateObj.minutes}, ${message.dateObj.day} ${message.dateObj.month}`}
-            </span>
-          </div>
-          <div className="messages__bottom">
-            <span className="messages__message">{message.message}</span>
-          </div>
-        </div>
+        <MessageContainer message={message} />
       ))}
     </div>
   );

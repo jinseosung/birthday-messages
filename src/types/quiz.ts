cea6dd7bee9097f690bf9d14c160c4968c31028e
Jinseo Sung <jinseosung15@gmail.com>
2024-01-26 12:24:04 +0100

    Add datas types

---
+++ src/types/quiz.ts
@@ -0,0 +1,5 @@
+export type Quiz = {
+  question: string;
+  answer: string[];
+  correctAnswer: string;
+};
---
+++ src/types/texts.ts
@@ -0,0 +1,17 @@
+export type Texts = {
+  id: number;
+  lanCode: string;
+  lan: string;
+  message: {
+    title: string;
+    from: string;
+    placeholder: string;
+    btn: string;
+  };
+  thx: {
+    title: string;
+    btn: string;
+    quiz: string;
+    main: string;
+  };
+};
